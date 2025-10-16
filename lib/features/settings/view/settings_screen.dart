49ae6e3f475e75497b9a9dfd48675918ed3cbf8b
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-10-16 20:23:04 +0300

    Доработан экран настроек

--- lib/features/settings/view/settings_screen.dart
+++ lib/features/settings/view/settings_screen.dart
@@ -9,6 +9,7 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -44,22 +45,66 @@ class SettingsScreen extends StatelessWidget {
             ),
           ),
           const SliverToBoxAdapter(child: SizedBox(height: 16)),
-          const SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: BaseContainer(
-                child: ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  leading: Icon(Icons.delete),
-                  title: Text('Очистить историю'),
-                  subtitle: Text(
-                    'История поиска хранится в кеше и его можно почистить',
-                  ),
-                ),
-              ),
+          SliverToBoxAdapter(
+            child: SettingsActionCard(
+              title: 'Очистить историю',
+              iconData: Icons.delete_sweep_outlined,
+              iconColor: theme.primaryColor,
+              onTap: () {},
+            ),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 8)),
+          SliverToBoxAdapter(
+            child: SettingsActionCard(
+              title: 'Поддержка',
+              iconData: Icons.message_outlined,
+              onTap: () {},
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SettingsActionCard extends StatelessWidget {
+  const SettingsActionCard({
+    super.key,
+    required this.title,
+    required this.iconData,
+    this.onTap,
+    this.iconColor,
+  });
+
+  final String title;
+  final IconData iconData;
+  final VoidCallback? onTap;
+  final Color? iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BaseContainer(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: theme.textTheme.titleMedium?.copyWith(fontSize: 18),
+              ),
+              Icon(
+                iconData,
+                size: 32,
+                color: iconColor ?? theme.hintColor.withAlpha(70),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
