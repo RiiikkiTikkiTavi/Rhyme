d85e2e69161e783a2cac2669ce23677839d7b10c
RiiikkiTikkiTavi <https://github.com/RiiikkiTikkiTavi>
2025-10-20 22:13:47 +0300

    Добавлен дизайн bottomSheet

--- lib/features/search/widgets/search_button.dart
+++ lib/features/search/widgets/search_button.dart
@@ -1,33 +1,38 @@
 import 'package:flutter/material.dart';
 
 class SearchButton extends StatelessWidget {
-  const SearchButton({super.key});
+  const SearchButton({super.key, required this.onTap});
+
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(12),
-      margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 8),
-      decoration: BoxDecoration(
-        //color: theme.hintColor.withValues(alpha: 10),
-        color: theme.hintColor.withValues(alpha: 0.1),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Row(
-        children: [
-          const Icon(Icons.search_rounded),
-          const SizedBox(width: 12),
-          Text(
-            "поиск рифм...",
-            style: TextStyle(
-              fontSize: 18,
-              color: theme.hintColor.withValues(alpha: 0.4),
-              fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 8),
+        decoration: BoxDecoration(
+          //color: theme.hintColor.withValues(alpha: 10),
+          color: theme.hintColor.withValues(alpha: 0.1),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          children: [
+            const Icon(Icons.search_rounded),
+            const SizedBox(width: 12),
+            Text(
+              'поиск рифм...',
+              style: TextStyle(
+                fontSize: 18,
+                color: theme.hintColor.withValues(alpha: 0.4),
+                fontWeight: FontWeight.w500,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
